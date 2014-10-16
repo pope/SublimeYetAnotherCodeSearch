40dd17b959ea5532d422c8fb0934792d38f1c29e
K. Adam Christensen <pope@shifteleven.com>
2014-10-15 20:34:24 -0700

    Add Index Project.

--- cindex.py
+++ cindex.py
@@ -34,7 +34,17 @@ class CindexCommand(sublime_plugin.WindowCommand, CindexListener):
     super(CindexCommand, self).__init__(*args, **kwargs)
     self._is_running = False
 
-  def run(self):
+  def run(self, index_project=False):
+    """Runs the cindex command.
+
+    Once this starts running, it will not listen to other calls until the index
+    is finished running.
+
+    Args:
+      index_project: If true, the index will be generated from folders used by
+          the project. The csearchindex file location must be set in the
+          project settings when set to True.
+    """
     if self._is_running:
       return
     self._is_running = True
@@ -47,14 +57,24 @@ class CindexCommand(sublime_plugin.WindowCommand, CindexListener):
 
     project_data = self.window.project_data()
     index_filename = None
+    paths_to_index = []
     if ('code_search' in project_data and
         'csearchindex' in project_data['code_search']):
       raw_index_filename = project_data['code_search']['csearchindex']
       index_filename = os.path.expanduser(raw_index_filename)
 
+      if index_project:
+        paths_to_index = [os.path.expanduser(folder['path'])
+                          for folder in project_data['folders']]
+
+    elif index_project:
+      sublime.error_message('Cannot index project. Missing csearchindex file')
+      return
+
     _CindexListThread(self,
                       path_cindex=path_cindex,
-                      index_filename=index_filename).start()
+                      index_filename=index_filename,
+                      paths_to_index=paths_to_index).start()
 
   def _increment_total_indexed(self, count):
     if not self._is_running:
@@ -82,11 +102,24 @@ class CindexCommand(sublime_plugin.WindowCommand, CindexListener):
 class _CindexListThread(threading.Thread):
   """Runs the cindex command in a thread."""
 
-  def __init__(self, listener, path_cindex='cindex', index_filename=None):
+  def __init__(self, listener, path_cindex='cindex', index_filename=None,
+               paths_to_index=None):
+    """Initializes the _CindexListThread.
+
+    Args:
+      listener: A _CindexListener object to send events to.
+      path_cindex: The location of the cindex command.
+      index_filename: An optional csearchindex file location to use.
+      paths_to_index: An optional list of paths to index. If supplied, replaces
+          the paths currently used in the csearchindex file.
+    """
     super(_CindexListThread, self).__init__()
     self._listener = listener
     self._path_cindex = path_cindex
     self._index_filename = os.path.abspath(index_filename)
+    self._paths_to_index = []
+    if paths_to_index:
+      self._paths_to_index = list(map(os.path.abspath, paths_to_index))
 
   def run(self):
     try:
@@ -97,7 +130,7 @@ class _CindexListThread(threading.Thread):
       self._listener.on_finished(err=e)
 
   def _check_index_file(self):
-    if not self._index_filename:
+    if not self._index_filename or self._paths_to_index:
       return
     if not os.path.isfile(self._index_filename):
       raise Exception(
@@ -119,6 +152,9 @@ class _CindexListThread(threading.Thread):
 
   def _start_indexing(self):
     cmd = [self._path_cindex, '-verbose']
+    if self._paths_to_index:
+      cmd.append('-reset')
+      cmd.extend(self._paths_to_index)
     proc = self._get_proc(cmd)
     start = time.time()
     count = 0
